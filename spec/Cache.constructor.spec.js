30a39b3298be21436b322da928fa65da4489503f
Jiri Chara <jiri@chara.vip>
2016-10-17 12:01:45 +0200

    Add Cache service

---
+++ spec/Cache.constructor.spec.js
@@ -0,0 +1,119 @@
+/* eslint import/no-extraneous-dependencies:0 */
+/* eslint no-param-reassign:0 */
+import test from 'ava';
+import sinon from 'sinon';
+
+import Cache from '../src/Cache';
+import FileProvider from '../src/cache/FileProvider';
+
+test.beforeEach((t) => {
+  t.context.string = 'Some string';
+  t.context.cache = new Cache({
+    string: t.context.string
+  });
+});
+
+test('initializes provider', (t) => {
+  t.true(
+    t.context.cache.provider instanceof FileProvider
+  );
+});
+
+test('initializes provider to given one', (t) => {
+  const provider = sinon.spy();
+  const cache = new Cache({
+    provider,
+    string: t.context.string
+  });
+
+  t.is(
+    cache.provider,
+    provider
+  );
+});
+
+test('throws an error if string was not given', (t) => {
+  t.throws(() => new Cache(), /Please specify a string that you want to cache./);
+});
+
+test('initializes string to given one', (t) => {
+  const string = 'Some string';
+
+  const cache = new Cache({
+    string
+  });
+
+  t.is(
+    cache.string,
+    string
+  );
+});
+
+test('initializes krtekOptions', (t) => {
+  t.deepEqual(
+    t.context.cache.krtekOptions,
+    {}
+  );
+});
+
+test('initializes krtekOptions to given one', (t) => {
+  const krtekOptions = sinon.spy();
+
+  const cache = new Cache({
+    string: t.context.string,
+    krtekOptions
+  });
+
+  t.is(
+    cache.krtekOptions,
+    krtekOptions
+  );
+});
+
+test('creates a cacheString', (t) => {
+  t.is(
+    t.context.cache.cacheString,
+    '{}Somestring'
+  );
+});
+
+test('creates a cacheString based on params', (t) => {
+  const cache = new Cache({
+    string: 'foo',
+    krtekOptions: {
+      foo: 1
+    }
+  });
+
+  t.is(
+    cache.cacheString,
+    '{"foo":1}foo'
+  );
+});
+
+test('generates cacheHash', (t) => {
+  t.is(
+    t.context.cache.cacheHash,
+    t.context.cache.generateHash(
+      t.context.cache.cacheString
+    )
+  );
+});
+
+test('generates numeric cacheHash', (t) => {
+  t.true(
+    /^-?\d+$/.test(t.context.cache.cacheHash)
+  );
+});
+
+test('generates unique cacheHash', (t) => {
+  const cache = new Cache({
+    string: 'foo',
+    krtekOptions: {
+      foo: 1
+    }
+  });
+
+  t.true(/^-?\d+$/.test(cache.cacheHash));
+  t.not(cache.cacheHash, t.context.cache.cacheHash);
+});
---
+++ src/Cache.js
@@ -0,0 +1,91 @@
+import { FileProvider } from './cache';
+import { CacheStringMissingError } from './errors';
+
+/**
+ * Cache service is responsible for caching. It uses FileProvider as a cache provider by default.
+ */
+export default class Cache {
+  constructor({
+    provider = new FileProvider({
+      folder: '/tmp'
+    }),
+    string,
+    krtekOptions = {}
+  } = {}) {
+    /**
+     * Caching provider. It must have set() and get() method.
+     */
+    this.provider = provider;
+
+    /**
+     * String that we would like to cache.
+     */
+    this.string = string;
+
+    if (!this.string) {
+      throw new CacheStringMissingError(
+        'Please specify a string that you want to cache.'
+      );
+    }
+
+    /**
+     * Options that have been passed to krtek and that are relevant for generation of unique hash.
+     */
+    this.krtekOptions = krtekOptions;
+
+    /**
+     * Unique cache string that is generated based on string and options passed to krtek.
+     */
+    this.cacheString = `
+      ${JSON.stringify(this.krtekOptions)}
+      ${this.string}
+    `;
+    this.cacheString = this.cacheString.replace(/\s/g, '');
+
+    /**
+     * Cache hash used for cache invalidation.
+     */
+    this.cacheHash = this.generateHash(this.cacheString);
+  }
+
+  /**
+   * Set cache.
+   */
+  set(callback) {
+    // TODO: handle errors
+    return this.provider.set(
+      this.cacheHash,
+      this.string,
+      callback
+    );
+  }
+
+  /*
+   * Get cache.
+   */
+  get(callback) {
+    // TODO: handle errors
+    return this.provider.get(
+      this.cacheHash,
+      callback
+    );
+  }
+
+  /*
+   * Generate a short hash based on string passed to it. It is used for cache invalidation.
+   */
+  generateHash(string) {
+    let hash = 0;
+    let i;
+    let chr;
+    let len;
+
+    if (string === 0) return hash;
+    for (i = 0, len = string.length; i < len; i++) {
+      chr = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0;
+    }
+    return hash;
+  }
+}
